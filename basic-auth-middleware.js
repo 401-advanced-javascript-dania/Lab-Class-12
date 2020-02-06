40504ebe973d5293de2bdd814d3bbc4fba8aa8f6
daniaalrababa9 <rababadania@gmail.com>
2020-02-06 12:37:54 +0200

    add

--- basic-auth-middleware.js
+++ basic-auth-middleware.js
@@ -1,13 +1,25 @@
+
 'use strict';
 const base64 = require('base-64');
-const users = require('./users.js');
-module.exports=(req,res,next) =>{
-    if(!req.headers.authorization) {next('Invalid Login'); return;}
-let basic = req.headers.authorization.split(' ').pop();
-let [user ,pass] = base64.decode(basic).split(':');
-users.authenticateBasic(user, pass)
- .then(validUser =>{
-     req.token = users.generateToken(validUser);
-     next();
- }) .catch(err=> next('Invalid Login'));
+const users = require('./index.js');
+//middle-ware function 
+let basicAuthMiddleware=function (req,res,next) {
+    //req.headers.authorization is hashed password and username
+    if(!req.headers.authorization){ next('cannot login'); return;}
+    // split is convert from string to an array 
+    let basicAuth = req.headers.authorization.split(' ').pop();
+    console.log('req.headers.authorization',req.headers.authorization)
+    console.log('basicAuth',basicAuth)
+    //['dania','12345']
+    let [user,password] = base64.decode(basicAuth).split(':');
+    console.log('[user,password] ',[user,password] )
+    // it will be comparing the password and it will return username
+    users.basicOfAuthenticate(user,password)
+    .then(goodUser=>{
+        // create a token to the request , the token will authorize me to access to specific route
+        req.token=users.tokenGeneration(goodUser)
+        console.log('token',req.token)
+        next();
+    }).catch(err=> next('cannot login'));
 }
+module.exports=basicAuthMiddleware;
