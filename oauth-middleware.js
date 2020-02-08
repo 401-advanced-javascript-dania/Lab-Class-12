9f26ed1a1197d6f5d60d9429dee2d21a4f861964
daniaalrababa9 <rababadania@gmail.com>
2020-02-08 21:32:33 +0200

    added

--- oauth-middleware.js
+++ oauth-middleware.js
@@ -7,9 +7,9 @@
 const superagent = require('superagent');
 const users = require('./users.js');
 // the url to get a token for access 
-const tokenUrl = 'http://github.com/login/oauth/access_token';
+const tokenUrl = 'https://github.com/login/oauth/access_token';
 // the api that we will be accessing 
-const remoteAPI = 'http://api.github.com/user';
+const remoteAPI = 'https://api.github.com/user';
 const CLIENT_ID='cee7d9c157365d5ea927';
 const CLIENT_SECRET='3e000384db3cb375703901435424d41b65fa8c3c';
 //our api server (path for get request ) redirect from github 
@@ -19,12 +19,14 @@ module.exports=async function authorize(req,res,next){
         let code = req.query.code;
         console.log('code',code)
         // give github access_token and give me user Info
-        let token = await codeAgainstToken(code);
+        let accessToken = await codeAgainstToken(code);
+        console.log('accessToken',accessToken)
         // take the user Info
-        let userLargeInfo = await getUserInfo(token);
+        let userLargeInfo = await getUserInfo(accessToken);
         // type the user Info and token 
         console.log('userLargeInfo',userLargeInfo)
         let [user,token] = await typeUserInfo(userLargeInfo);
+        console.log('[user,token]',[user,token])
         req.user=user;
         req.token=token;
         next();
@@ -49,15 +51,16 @@ async function codeAgainstToken(code){
 async function getUserInfo(token){
     let userRes = await superagent.get(remoteAPI)
     //set headers
-    .set('user-agent','express-app')
-    .set('Authorization',`token${token}`)
+    .set('user-agent', 'express-app')
+    .set('Authorization', `token ${token}`)
+    // console.log('userRes',userRes)
 let userInfo = userRes.body;
 return userInfo;
 }
 // it type the user information , the user that own that github 
-async function typeUserInfo(user){
+async function typeUserInfo(userName){
     let userData= {
-        username:user.login,
+        username:userName.login,
         password:'anything'
     }
     let user = await users.userInfoSave(userData);
